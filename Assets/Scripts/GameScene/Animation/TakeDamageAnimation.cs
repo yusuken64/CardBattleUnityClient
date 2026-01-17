090ac2e8e30e705bee52f94f388a4a48c371b324
yusuken64 <61254107+yusuken64@users.noreply.github.com>
2026-01-16 20:19:40 -0500

    aoe context

--- Assets/Scripts/GameScene/Animation/TakeDamageAnimation.cs
+++ Assets/Scripts/GameScene/Animation/TakeDamageAnimation.cs
@@ -1,6 +1,8 @@
 ﻿using CardBattleEngine;
 using DG.Tweening;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TakeDamageAnimation : GameActionAnimation<DamageAction>
@@ -9,44 +11,58 @@ public class TakeDamageAnimation : GameActionAnimation<DamageAction>
 	public GameObject DamageParticles;
 	public override IEnumerator Play()
 	{
-		Common.Instance.AudioManager.PlaySound(DamageSound);
-		GameObject gameObject = GameManager.GetObjectFor(Context.Target);
-
-		if (gameObject is null)
+		List<(IGameEntity, int)> targets;
+		if (Context.AffectedEntities != null &
+			Context.AffectedEntities.Count() > 0)
 		{
-			throw new System.Exception("No damage object");
+			targets = Context.AffectedEntities.ToList();
+		}
+		else
+		{
+			targets = new() { (Context.Target, Context.DamageDealt) };
 		}
 
-		Transform target = gameObject.transform;
-
-		var particles = Instantiate(DamageParticles, target.transform.position, Quaternion.identity);
-
-		// simple shake: short, small, no fancy stuff
-		Tween shake = target.DOShakePosition(
-			duration: 0.2f,
-			strength: 0.15f,   // how far it shakes
-			vibrato: 20,       // how fast it vibrates
-			randomness: 90f,   // random direction
-			fadeOut: true
-		);
-
-		Object.FindFirstObjectByType<UI>().ShowDamage((Context.DamageDealt), target);
-
-		yield return shake.WaitForCompletion();
-
-		var portrait = gameObject.GetComponent<HeroPortrait>();
-		var minion = gameObject.GetComponent<Minion>();
-		if (portrait != null)
+		foreach (var target in targets)
 		{
-			portrait.Player.Health -= Context.DamageDealt;
-			portrait.Player.Armor -= Context.ArmorDamageDealt;
-			portrait.Player.UpdateUI();
-		}
-		else if (minion != null)
-		{
-			minion.Health -= Context.DamageDealt;
-			minion.HasDivineShield = (Context.Target as CardBattleEngine.Minion).HasDivineShield;
-			minion.UpdateUI();
+			GameObject gameObject = GameManager.GetObjectFor(target.Item1);
+
+			if (gameObject is null)
+			{
+				throw new System.Exception("No damage object");
+			}
+
+			Transform targetTransform = gameObject.transform;
+
+			var particles = Instantiate(DamageParticles, targetTransform.transform.position, Quaternion.identity);
+
+			// simple shake: short, small, no fancy stuff
+			Tween shake = targetTransform.DOShakePosition(
+				duration: 0.2f,
+				strength: 0.15f,   // how far it shakes
+				vibrato: 20,       // how fast it vibrates
+				randomness: 90f,   // random direction
+				fadeOut: true
+			);
+
+			Common.Instance.AudioManager.PlaySound(DamageSound);
+			Object.FindFirstObjectByType<UI>().ShowDamage(target.Item2, targetTransform);
+
+			yield return shake.WaitForCompletion();
+
+			//var portrait = gameObject.GetComponent<HeroPortrait>();
+			//var minion = gameObject.GetComponent<Minion>();
+			//if (portrait != null)
+			//{
+			//	portrait.Player.Health -= Context.DamageDealt;
+			//	portrait.Player.Armor -= Context.ArmorDamageDealt;
+			//	portrait.Player.UpdateUI();
+			//}
+			//else if (minion != null)
+			//{
+			//	minion.Health -= Context.DamageDealt;
+			//	minion.HasDivineShield = (Context.Target as CardBattleEngine.Minion).HasDivineShield;
+			//	minion.UpdateUI();
+			//}
 		}
 	}
 }
--- Assets/Scripts/GameScene/Animation/TakeHealAnimation.cs
+++ Assets/Scripts/GameScene/Animation/TakeHealAnimation.cs
@@ -1,30 +1,45 @@
 ﻿using CardBattleEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TakeHealAnimation : GameActionAnimation<HealAction>
 {
 	public override IEnumerator Play()
 	{
-		GameObject gameObject = GameManager.GetObjectFor(Context.Target);
-		if (gameObject == null) { yield break; }
-		Transform target = gameObject.transform;
-
-		Object.FindFirstObjectByType<UI>().ShowHeal(Context.HealedAmount, target);
-
-		var portrait = gameObject.GetComponent<HeroPortrait>();
-		var minion = gameObject.GetComponent<Minion>();
-		if (portrait != null)
+		List<(IGameEntity, int)> targets;
+		if (Context.AffectedEntities != null &
+			Context.AffectedEntities.Count() > 0)
 		{
-			portrait.Player.Health += Context.HealedAmount;
-			portrait.Player.UpdateUI();
+			targets = Context.AffectedEntities.ToList();
 		}
-		else if (minion != null)
+		else
 		{
-			minion.Health += Context.HealedAmount;
-			minion.UpdateUI();
+			targets = new() { (Context.Target, Context.DamageDealt) };
 		}
 
+		foreach (var target in targets)
+		{
+			GameObject gameObject = GameManager.GetObjectFor(target.Item1);
+			if (gameObject == null) { yield break; }
+			Transform targetTransforms = gameObject.transform;
+
+			Object.FindFirstObjectByType<UI>().ShowHeal(target.Item2, targetTransforms);
+
+			//var portrait = gameObject.GetComponent<HeroPortrait>();
+			//var minion = gameObject.GetComponent<Minion>();
+			//if (portrait != null)
+			//{
+			//	portrait.Player.Health += Context.HealedAmount;
+			//	portrait.Player.UpdateUI();
+			//}
+			//else if (minion != null)
+			//{
+			//	minion.Health += Context.HealedAmount;
+			//	minion.UpdateUI();
+			//}
+		}
 		yield return null;
 	}
 }
