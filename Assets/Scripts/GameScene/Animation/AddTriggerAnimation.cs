e093a567854c112ce9ec4cec27db7d2dc4b1b85b
yusuken64 <61254107+yusuken64@users.noreply.github.com>
2026-02-28 01:25:05 -0500

    add trigger animation

---
+++ Assets/Scripts/GameScene/Animation/AddTriggerAnimation.cs
@@ -0,0 +1,10 @@
+﻿using CardBattleEngine;
+using System.Collections;
+
+public class AddTriggerAnimation : GameActionAnimation<AddTriggerAction>
+{
+	public override IEnumerator Play()
+	{
+		yield return null;
+	}
+}
