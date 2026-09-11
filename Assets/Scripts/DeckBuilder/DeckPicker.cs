3fbb96e5d017c41b189b8b8f3e279db15f3cdf5f
YusukeHasegawaTylerTech <yusuke.hasegawa@tylertech.com>
2026-09-11 14:34:22 -0400

    checkpoint: deck-crud-active-deck before wave 4

--- Assets/Scripts/DeckBuilder/DeckPicker.cs
+++ Assets/Scripts/DeckBuilder/DeckPicker.cs
@@ -2,16 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DeckPicker : MonoBehaviour
 {
 	public DeckPickerButton DeckPickerButtonPrefab;
 	public Transform DeckPickerButtonContainer;
 	public List<DeckPickerButton> DeckPickerButtons;
+	public Button AddDeckButton;
 
 	public Action<DeckPickerButton> DeckPickedAction { get; internal set; }
+	public Action<DeckPickerButton> DeckSelectedAction { get; internal set; }
+	public Action<DeckPickerButton> DeleteRequestedAction { get; internal set; }
+	public bool SelectMode;
 
 	private void Start()
+	{
+		Rebuild();
+	}
+
+	public void Rebuild()
 	{
 		foreach (Transform transform in DeckPickerButtonContainer)
 		{
@@ -19,15 +29,44 @@ public class DeckPicker : MonoBehaviour
 		}
 		DeckPickerButtons.Clear();
 
+		var activeDeckID = Common.Instance.SaveManager.SaveData.GameSaveData.ActiveDeckID;
+
 		foreach (var deckData in Common.Instance.SaveManager.SaveData.GameSaveData.DeckSaveDatas)
 		{
 			var deckPickerButton = Instantiate(DeckPickerButtonPrefab, DeckPickerButtonContainer);
 			deckPickerButton.DeckPickedAction = DeckPickedAction;
+			deckPickerButton.DeckSelectedAction = DeckSelectedAction;
+			deckPickerButton.DeleteRequestedAction = DeleteRequestedAction;
+			deckPickerButton.SelectMode = SelectMode;
 
 			deckPickerButton.Setup(deckData);
+			deckPickerButton.SetActiveHighlight(deckData.ID == activeDeckID);
 			DeckPickerButtons.Add(deckPickerButton);
 		}
+
+		if (AddDeckButton != null)
+			AddDeckButton.gameObject.SetActive(!SelectMode);
+
 		UpdateUI();
+	}
+
+	public void AddDeck_Clicked()
+	{
+		var newDeckData = new DeckSaveData
+		{
+			ID = Guid.NewGuid().ToString(),
+			Title = "New Deck",
+			CardIDs = new List<string>()
+		};
+
+		Common.Instance.SaveManager.SaveData.GameSaveData.DeckSaveDatas.Add(newDeckData);
+		Common.Instance.SaveManager.Save();
+
+		Rebuild();
+
+		var newButton = DeckPickerButtons.FirstOrDefault(b => b.DeckID == newDeckData.ID);
+		if (newButton != null)
+			DeckPickedAction?.Invoke(newButton);
 	}
 
 	public void UpdateUI()
